c6853644e76f1f26361a34dd76010a46e1d1c5b3
Haifeng Jiang <haifeng.jiang@exxonmobil.com>
2020-06-29 22:33:40 +0800

    solve practice2.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,18 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+    let {data, column} = input;
+    let array = [];
+    for (let item of column) {
+        array.push(item.name);
+    }
+    let result=[];
+    for (let item of data) {
+        let obj = {};
+        for (let i in item) {
+            obj[array[i]] = item[i];
+        }
+        result.push(obj);
+    }
+    return result;
+};
+
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,15 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+    console.log(sections);
+    sections.sort((a, b) => {
+            return b.index - a.index
+        }
+    );
+    console.log(sections);
+    for (let section of sections) {
+        let {content, index} = section;
+        items.splice(index, 0, content);
+    }
+    return items;
+};
+
+export {inject};
